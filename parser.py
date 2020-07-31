45293ae1d5b7635f7aa6258493fe4e18a398b6f5
lang8 <qwerty67852@gmail.com>
2020-07-31 18:57:19 +0900

    update for iteration

--- parser.py
+++ parser.py
@@ -9,8 +9,16 @@ import json, os, time
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 pattern = compile('[<script type="text/javascript">\nvar jsonResultData={}\nvar f9DataList={}')
+
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+option.add_argument('window-size=1920x1080')
+option.add_argument("disable-gpu")
+driver = webdriver.Chrome(os.path.join(BASE_DIR,'driver/chromedriver.exe'), options=option)
+'''
 # chrome driver와 chrome browser 모두 84버전을 사용했는지 확인
 driver = webdriver.Chrome(os.path.join(BASE_DIR,'driver/chromedriver.exe'))
+'''
 driver.implicitly_wait(3)
 # frameset회피를 위해 직접 링크 이용
 driver.get('http://portal.kleague.com/user/loginById.do?portalGuest=rstNE9zxjdkUC9kbUA08XQ==')
@@ -20,19 +28,19 @@ driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[1]/div[2]/ul/li/a
 #초크보드 사전 선택을 위해서 먼저 시작 값 선택
 driver.find_element_by_xpath("//select[@name='meetYear']/option[text()='2019']").click()
 driver.find_element_by_xpath("//select[@name='meetSeq']/option[@value='1']").click()
-driver.find_element_by_xpath("//select[@name='roundId']/option[text()='1']".format(1)).click()
-driver.find_element_by_xpath("//select[@name='gameId']/option[@value=1]".format(1)).click()
+driver.find_element_by_xpath("//select[@name='roundId']/option[text()='1']").click()
+driver.find_element_by_xpath("//select[@name='gameId']/option[@value=1]").click()
 time.sleep(5)
 driver.find_element_by_xpath('//*[@id="contentsLayer"]/div/div/ul/li[7]/a').click()
-for round in range(1,2):
-    driver.find_element_by_xpath("//select[@name='meetYear']/option[text()='2019']").click()
-    driver.find_element_by_xpath("//select[@name='meetSeq']/option[@value='1']").click()
-    driver.find_element_by_xpath("//select[@name='roundId']/option[text()='{}']".format(round)).click()
-    for team in range(1,3):
+team = 0
+for round in range(1,39):
+    driver.find_element_by_xpath("//select[@name='roundId']/option[@value={}]".format(round)).click()
+    for i in range(1,7):
         #드롭다운박스 설정
+        team += 1
         driver.find_element_by_xpath("//select[@name='gameId']/option[@value={}]".format(team)).click()
         #체크박스 설정
-        driver.find_element_by_xpath('//*[@id="checkContent_ST"]').click()
+        #driver.find_element_by_xpath('//*[@id="checkContent_ST"]').click()
 
         #파싱 파트
         html = driver.page_source
@@ -47,5 +55,6 @@ for round in range(1,2):
         json_data = [play for play in json_data if "TYPE_CD" in play and play["TYPE_CD"] == "ST"]
         # 파일 저장
         textround = '0' + str(round) if round < 10 else str(round)
-        with open(os.path.join(BASE_DIR, 'shoot_info_91{}{}.json'.format(textround,team)), 'w+') as json_file:
+        with open(os.path.join(BASE_DIR, 'shoot_info_91{}{}.json'.format(textround,team%6)), 'w+') as json_file:
             json.dump(json_data, json_file)
+driver.quit()
--- readjson.py
+++ readjson.py
@@ -2,7 +2,7 @@ import json
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-with open(os.path.join(BASE_DIR, 'result.json'), 'r') as f:
+with open(os.path.join(BASE_DIR, 'shoot_info_91011.json'), 'r') as f:
     json_data = json.load(f)
     #json_data = [play for play in json_data if "TYPE_CD" in play and play["TYPE_CD"] == "ST"]
 print(json.dumps(json_data, indent ='\t'))
